5ad6656efb71003c1418f7d116fa70be8292ea76
aji <aji@enfintechnologies.com>
2019-09-06 16:54:33 +0530

    full screen mode

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, globalShortcut} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
+function minimizeWindow() {
+  mainWindow.setFullScreen(false);
+}
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -13,33 +15,28 @@ function createWindow () {
     height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    fullscreen:true,
+    fullscreenable:true
   })
   mainWindow.maximize()
   mainWindow.webContents.openDevTools()
   let url = process.argv.slice(2, process.argv.length)
   if (url.length > 0) url = url[0]
   else url = "https://testingapp.spjain.org/"
-  console.log(url)
-  // and load the index.html of the app.
+
   mainWindow.loadURL(url)
-  // mainWindow.loadURL('https://videowall.spjain.org/wall?roomID=SPJR1003&sessionId=5d64d756c2944912381ae83d&mcuRoom=5d09b6a523692b4fe4ffd27c&location=km&vwLocation=km')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
   })
+  const ret = globalShortcut.register('Escape', function () {
+    console.log('key is pressed');
+    minimizeWindow();
+  });
+  mainWindow.webContents.on('console-message', (event, level, message, line, sourceId) => {
+    console.log(message + " " + sourceId + " (" + line + ")");
+  });
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
@@ -54,6 +51,13 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow()
 })
+
+app.on('will-quit', function () {
+  console.log('will-quit')
+  globalShortcut.unregister('Escape');
+
+  globalShortcut.unregisterAll();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
