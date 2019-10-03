3ccf8e823e406f797601785c0e034dd8a8317c00
aji <aji@enfintechnologies.com>
2019-10-03 11:47:08 +0530

    full screen toggling

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const electronLocalshortcut = require('electron-localshortcut');
 let mainWindow
 let child
 let url
+let moderatorWindow
 function minimizeWindow() {
   mainWindow.setFullScreen(false);
   mainWindow.unmaximize()
@@ -17,7 +18,6 @@ autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
 function createWindow() {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 1080,
     height: 720,
@@ -25,9 +25,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       backgroundThrottling: false
-    },
-    fullscreen: true,
-    fullscreenable: true
+    }
   })
   url = process.argv.slice(2, process.argv.length)
   if (url.length > 0) url = url[0]
@@ -37,43 +35,31 @@ function createWindow() {
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+  mainWindow.setMenuBarVisibility(false)
+  mainWindow.setFullScreen(true)
   mainWindow.on('maximize', function () {
-    console.log('maximize')
-    if (child == null) mainWindow.setFullScreen(true)
+    // if (child == null){
+    //   mainWindow.setFullScreen(true)
+    // }
   })
   subscribeRenderEvents()
+
   electronLocalshortcut.register(mainWindow, 'Escape', ()=>{
     minimizeWindow()
   })
+
   electronLocalshortcut.register(mainWindow, 'F5', ()=>{
-    mainWindow.webContents.reload()
+    mainWindow.setFullScreen(false);
   })
+  electronLocalshortcut.register(mainWindow, 'F11', ()=>{
+    mainWindow.setMenu(null)
+    if (mainWindow.isFullScreen()) mainWindow.setFullScreen(false)
+    else mainWindow.setFullScreen(true)
+  })
+
   electronLocalshortcut.register(mainWindow, 'CommandOrControl+Shift+J', ()=>{
     mainWindow.webContents.openDevTools()
   })
-
-  autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-  })
-  autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
-  })
-  autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
-  })
-  autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
-  })
-  autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
-  })
-  autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
-  });
-
 }
 app.on('ready', createWindow)
 
@@ -148,7 +134,78 @@ function subscribeRenderEvents() {
       mainWindow.webContents.send('screenshare-stoped-from-ec')
     })
    
-    // child.focus()
+  })
+  ipcMain.on('moderator-enter-to-splitroom', function (msg, store) {
+    console.log(store)
+    if (moderatorWindow != undefined) {
+      // electronLocalshortcut.unregister(moderatorWindow, 'Escape');
+      electronLocalshortcut.unregister(moderatorWindow, 'CommandOrControl+Shift+J');
+      electronLocalshortcut.unregisterAll(moderatorWindow, 'F5');
+      electronLocalshortcut.unregisterAll(moderatorWindow);
+      moderatorWindow.close()
+      moderatorWindow = null
+    }
+    let url = store.msg.url
+    let screen = require('electron').screen
+    let displays = screen.getAllDisplays();
+    let height = 0;
+    let width = 0;
+    for (var i in displays) {
+      height += displays[i].bounds.height;
+      width += displays[i].bounds.width;
+    }
+    mainWindow.setFullScreen(false);
+    mainWindow.blur()
+    moderatorWindow = new BrowserWindow({
+      width: width - 100,
+      height: height -100,
+      // parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true,
+        backgroundThrottling: false
+      }
+    })
+    moderatorWindow.loadURL(url, { "extraHeaders": "pragma: no-cache\n" })
+    moderatorWindow.once('ready-to-show', () => {
+      moderatorWindow.show()
+    })
+    moderatorWindow.on('closed', () => {
+      moderatorWindow = null
+      mainWindow.webContents.send('moderator-enter-to-splitroom-ended')
+    })
+
+
+    electronLocalshortcut.register(moderatorWindow, 'Escape', () => {
+      // minimizeWindow()
+    })
+    electronLocalshortcut.register(moderatorWindow, 'F5', () => {
+      mainWindow.webContents.reload()
+    })
+    electronLocalshortcut.register(moderatorWindow, 'CommandOrControl+Shift+J', () => {
+      mainWindow.webContents.openDevTools()
+    })
   })
 }
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
 
